6e60bc9225d293410e5a13d3429d818a3b0d1696
LuanOliveiraCampos <108338260+LuanOliveiraCampos@users.noreply.github.com>
2024-05-27 22:32:08 -0300

    Create ProdutoDAO.java

---
+++ ProdutoDAO.java
@@ -0,0 +1,29 @@
+package com.example.crud;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+    private static final String FILE_NAME = "src/main/resources/produtos.txt";
+
+    public static List<Produto> loadProdutos() {
+        List<Produto> produtos = new ArrayList<>();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            produtos = (List<Produto>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            // Arquivo não encontrado, retorna lista vazia
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return produtos;
+    }
+
+    public static void saveProdutos(List<Produto> produtos) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(produtos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
